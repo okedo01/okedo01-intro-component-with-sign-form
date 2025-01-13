5095a71ca93d8fc931fee0227e3c8bc04650d317
Eliah <eliahmwelangi01@gmail.com>
2025-01-13 10:55:18 +0300

    adding javascript to the form

--- public/sign-up.js
+++ public/sign-up.js
@@ -141,14 +141,30 @@
 
 
 const contactForm = document.querySelector('form');
-let namePattern = /^[a-z0-9]{6,}$/;
+const namePattern = /^[a-z0-9]{6,}$/;
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!#%&?*])[A-Za-z\d!@#$%&*?]{8,}$/;
 
 contactForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    validateFirstName();
-    validateLastName();
-    validateEmail();
-    validatePassword();
+
+    let isValid = true;
+    const validators = [
+        validateFirstName,
+        validateLastName,
+        validateEmail,
+        validatePassword
+    ]
+    
+    validators.forEach(validate => {
+        if(!validate) {
+            isValid = false;
+        }
+        // contactForm.reset();
+    })
+    if(isValid) {
+        contactForm.reset();
+    }
 })
 
 const validateFirstName = () => {
@@ -156,62 +172,93 @@ const validateFirstName = () => {
     if(firstName === "") {
         document.querySelector('.first-img').style.display = 'inline';
         document.querySelector('.first').innerHTML = "First Name can not be empty";
+        contactForm.first.setAttribute('id', 'failed');
     }else if(!namePattern.test(firstName)) {
         document.querySelector('.first').innerHTML = "First Name must be 6 characters long";
     }else {
         document.querySelector('.first-img').style.display = 'none';
         document.querySelector('.first').innerHTML = "";
+        contactForm.first.setAttribute('id', 'success');
     }
-
-    // firstName.addEventListener('keyup', event => {
-    //     if(namePattern.test(event.target.value)) {
-    //         console.log("passed");
-    //         contactForm.firstName.setAttribute('id', 'success');
-            
-    //     }else {
-    //         console.log("failed");
-    //         contactForm.firstName.setAttribute('id', 'failed');
-    //     }
-    // })
 }
+contactForm.first.addEventListener('keyup', event => {
+    if(namePattern.test(event.target.value)) {
+        contactForm.first.setAttribute('id', 'success');
+        
+    }else {
+        contactForm.first.setAttribute('id', 'failed');
+    }
+})
 
 const validateLastName = () => {
     const lastName = contactForm.last.value.trim();
     if(lastName === "") {
         document.querySelector('.last-img').style.display = 'inline';
         document.querySelector('.last').innerHTML = "Last Name can not be empty";
+        contactForm.last.setAttribute('id', 'failed');
     }else if(!namePattern.test(lastName)) {
         document.querySelector('.last').innerHTML = "Last Name must be 6 characters long";
     }else {
         document.querySelector('.last-img').style.display = 'none';
         document.querySelector('.last').innerHTML = "";
+        contactForm.last.setAttribute('id', 'success');
     }
 }
+contactForm.last.addEventListener('keyup', event => {
+    if(namePattern.test(event.target.value)) {
+        contactForm.last.setAttribute('id', 'success');
+        
+    }else {
+        contactForm.last.setAttribute('id', 'failed');
+    }
+})
 
 const validateEmail = () => {
     const email = contactForm.email.value.trim();
-    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if(email === "") {
         document.querySelector('.email-img').style.display = 'inline';
         document.querySelector('.email').innerHTML = "Email can not be empty";
-    }else if(!pattern.test(email)) {
+        contactForm.email.setAttribute('id', 'failed');
+    }else if(!emailPattern.test(email)) {
         document.querySelector('.email').innerHTML = "Looks like this is not an email";
     }else {
         document.querySelector('.email-img').style.display = 'none';
         document.querySelector('.email').innerHTML = "";
+        contactForm.email.setAttribute('id', 'success');
     }
 }
+contactForm.email.addEventListener('keyup', event => {
+    if(emailPattern.test(event.target.value)) {
+        console.log("passed");
+        contactForm.email.setAttribute('id', 'success');
+        
+    }else {
+        console.log("failed");
+        contactForm.email.setAttribute('id', 'failed');
+    }
+})
 
 const validatePassword = () => {
     const password = contactForm.password.value.trim();
-    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!#%&?*])[A-Za-z\d!@#$%&*?]{8,}$/;
     if(password === "") {
         document.querySelector('.password-img').style.display = 'inline';
         document.querySelector('.password').innerHTML = "Password can not be empty";
+        contactForm.password.setAttribute('id', 'failed');
     }else if(!passwordPattern.test(password)) {
         document.querySelector('.password').innerHTML = "Password must be at least 8 letters icluding special characters, lowercase, uppercase and numbers eg. Okedo123@";
     }else {
         document.querySelector('.password-img').style.display = 'none';
         document.querySelector('.password').innerHTML = "";
+        contactForm.password.setAttribute('id', 'success');
     }
 }
+contactForm.password.addEventListener('keyup', event => {
+    if(passwordPattern.test(event.target.value)) {
+        console.log("passed");
+        contactForm.password.setAttribute('id', 'success');
+        
+    }else {
+        console.log("failed");
+        contactForm.password.setAttribute('id', 'failed');
+    }
+})
